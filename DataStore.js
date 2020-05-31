4401b7479b6f8c22d06d1a1536e1d2074055f484
itsaif <saif@envoymobile.com>
2020-06-01 02:49:25 +0500

    converted data store to singelton , fix the onclick error, added debounce, longer than 255 char will be truncated

--- DataStore.js
+++ DataStore.js
@@ -1,48 +1,64 @@
-'use strict'
+'use strict';
 
-const Store = require('electron-store')
+const Store = require('electron-store');
 
 class DataStore extends Store {
-
-  constructor (settings) {
-    super(settings)
-    this.name=settings.name;
+  constructor(settings) {
+    super(settings);
+    this.name = settings.name;
     // initialize with items or empty array
-    this.items = this.get(this.name) || []
+    this.items = this.get(this.name) || [];
   }
 
-  getall(){
-    return this.items
+  getall() {
+    return this.items;
   }
-  clear(){
-    this.set(this.name,[])
+  clear() {
+    this.set(this.name, []);
   }
-  saveitems () {
+  saveitems() {
     // save items to JSON file
-    this.set(this.name, this.items)
+    this.set(this.name, this.items);
 
     // returning 'this' allows method chaining
-    return this
+    return this;
   }
 
-  getitems (key) {
+  getitems(key) {
     // set object's items to items in JSON file
-    return this.items.find(t => t.hash == key)
+    return this.items.find((t) => t.hash == key);
   }
 
-  additem (item) {
+  additem(item) {
     // merge the existing items with the new item
-    this.items.push(item)
+    this.items.push(item);
 
-    return this.saveitems()
+    return this.saveitems();
   }
 
-  deleteitem (item) {
+  deleteitem(item) {
     // filter out the target item
-    this.items = this.items.filter(t => t !== item)
+    this.items = this.items.filter((t) => t !== item);
 
-    return this.saveitems()
+    return this.saveitems();
   }
 }
+var Singleton = (function () {
+  var instance;
 
-module.exports = DataStore
+  function createInstance() {
+    var object = new DataStore({ name: 'Items Main' });
+    return object;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+module.exports = Singleton;
